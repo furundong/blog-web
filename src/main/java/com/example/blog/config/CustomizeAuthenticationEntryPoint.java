9f224522a1bd50a1a151993917c3544aeb9d9aa8
furundong <2473579100@qq.com>
2020-04-10 08:21:30 +0800

    add permission fix

---
+++ src/main/java/com/example/blog/config/CustomizeAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.example.blog.config;
+
+import com.alibaba.fastjson.JSON;
+import com.example.blog.entity.response.Result;
+import com.example.blog.entity.response.ResultCode;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: Hutengfei
+ * @Description: 匿名用户访问无权限资源时的异常
+ * @Date Create in 2019/9/3 21:35
+ *
+ * AuthenticationEntryPoint主要是用来处理匿名用户访问无权限资源时的异常（即未登录，或者登录状态过期失效）
+ */
+@Component
+public class CustomizeAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        Result result = new Result(ResultCode.FAIL);
+        httpServletResponse.setContentType("text/json;charset=utf-8");
+        httpServletResponse.getWriter().write(JSON.toJSONString(result));
+    }
+}
--- src/main/java/com/example/blog/config/WebSecurityConfig.java
+++ src/main/java/com/example/blog/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.blog.config;
 
 import com.alibaba.fastjson.JSON;
-import com.example.blog.entity.SysUser;
 import com.example.blog.entity.response.Result;
 import com.example.blog.entity.response.ResultCode;
 import com.example.blog.service.SysUserService;
@@ -14,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.annotation.Resource;
@@ -31,20 +31,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     PasswordEncoder passwordEncoder;
 
+    @Resource
+    AuthenticationEntryPoint authenticationEntryPoint;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);;
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
     }
 
     @Bean
     LoginFilter loginFilter() throws Exception {
         LoginFilter loginFilter = new LoginFilter();
         loginFilter.setAuthenticationSuccessHandler((request, response, authentication) -> {
+            // 更新用户表上次登录时间、更新人、更新时间等字段
+            // User userDetails = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
             response.setContentType("application/json;charset=utf-8");
             PrintWriter out = response.getWriter();
-            SysUser sysUser = (SysUser) authentication.getPrincipal();
-            sysUser.setPassword(null);
-            String result = JSON.toJSONString(new Result(ResultCode.SUCCESS, sysUser));
+            Object principal = authentication.getPrincipal();
+
+            //此处还可以进行一些处理，比如登录成功之后可能需要返回给前台当前用户有哪些菜单权限，
+            //进而前台动态的控制菜单的显示等，具体根据自己的业务需求进行扩展
+
+//            principal.setPassword(null);
+            String result = JSON.toJSONString(new Result(ResultCode.SUCCESS, principal));
             out.write(result);
             out.flush();
             out.close();
@@ -59,10 +68,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 respBean.setMessage("密码过期，请联系管理员!");
             } else if (exception instanceof AccountExpiredException) {
                 respBean.setMessage("账户过期，请联系管理员!");
+            } else if (exception instanceof InternalAuthenticationServiceException) {
+                respBean.setMessage("用户不存在!"); // 这个其实已经在userService已经写了
             } else if (exception instanceof DisabledException) {
                 respBean.setMessage("账户被禁用，请联系管理员!");
             } else if (exception instanceof BadCredentialsException) {
                 respBean.setMessage("用户名或者密码输入错误，请重新输入!");
+            } else {
+                //其他错误
+                respBean.setMessage("登录失败");
             }
             out.write(new ObjectMapper().writeValueAsString(respBean));
             out.flush();
@@ -76,15 +90,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").access("hasRole('admin')")
-                .antMatchers("/admin/**").permitAll()///admin/**的URL都需要有超级管理员角色，如果使用.hasAuthority()方法来配置，需要在参数中加上ROLE_,如下.hasAuthority("ROLE_超级管理员")
+                .antMatchers("/**").permitAll()
+//                .antMatchers("/admin/**").permitAll()///admin/**的URL都需要有超级管理员角色，如果使用.hasAuthority()方法来配置，需要在参数中加上ROLE_,如下.hasAuthority("ROLE_超级管理员")
                 .anyRequest().authenticated()//其他的路径都是登录后即可访问
-                .and().logout().permitAll().and().csrf().disable();
+                //异常处理(权限拒绝、登录失效等)
+                .and().exceptionHandling().
+                authenticationEntryPoint(authenticationEntryPoint)//匿名用户访问无权限资源时的异常处理
+                .and().logout().permitAll().logoutSuccessHandler((req, resp, auth) ->{
+                    resp.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = resp.getWriter();
+                    out.write(JSON.toJSONString(new Result(ResultCode.SUCCESS)));
+                    out.flush();
+                    out.close();
+        }).deleteCookies("JSESSIONID").and().csrf().disable();  // todo have some problem
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/blog/img/**","/index.html","/static/**","/favicon.ico");
+        web.ignoring().antMatchers("/blog/img/**", "/index.html", "/static/**", "/favicon.ico");
     }
 
     @Bean
